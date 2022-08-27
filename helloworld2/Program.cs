902378115d676eb603f123220d3e6cf26625392a
afalkenberry <101435646+afalkenberry@users.noreply.github.com>
2022-08-26 23:12:17 -0400

    Add project files.

---
+++ helloworld2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+string stg = "Hello World! ";
+Console.WriteLine(stg);
+Console.ReadLine();
+
+Console.WriteLine("Enter Username");
+string userName = Console.ReadLine();
+Console.WriteLine(userName);
+Console.WriteLine("Username is: " + userName);
+Console.ReadLine();
